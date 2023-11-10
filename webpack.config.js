992bfae4c8af14eefa39c310ea53a728080a76fa
Davy <100140271+DavyR01@users.noreply.github.com>
2023-11-10 23:26:19 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    mainEntry : path.resolve(__dirname, "src/index.js"), // Important
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html") // Important
+    })
+  ],
+  // stats: "minimal",
+  devtool: "source-map",
+  mode: "development",
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+    open: true,
+    watchFiles: ['./src/**'], // Important
+    port: 4000,
+    hot: true,
+  }
+};
