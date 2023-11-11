17f84e99451df21f4a3f47f4eb1d594b8862a005
Davy <100140271+DavyR01@users.noreply.github.com>
2023-11-11 20:44:35 +0100

    delete folder dist

--- dist/topbar.bundle.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!******************************************!*\
-  !*** ./src/assets/javascripts/topbar.js ***!
-  \******************************************/
-console.log('top bar');
-/******/ })()
-;
-//# sourceMappingURL=topbar.bundle.js.map
