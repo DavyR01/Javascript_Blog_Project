b39a56cda2d8ed72a9d7ce3a605f9b016e86a354
Davy <100140271+DavyR01@users.noreply.github.com>
2024-11-04 21:30:43 +0100

    Chapitre 16: Leçon 130 - Rendre le projet responsive

--- src/assets/javascripts/topbar.js
+++ src/assets/javascripts/topbar.js
@@ -1,1 +1,55 @@
 console.log('top bar');
+
+const iconMobile = document.querySelector(".header-menu-icon");
+const headerMenu = document.querySelector(".header-menu");
+let isMenuOpen = false;
+let mobileMenuDOM;
+
+const closeMenu = () => {
+   mobileMenuDOM.classList.remove("open");
+};
+
+const createMobileMenu = () => {
+   mobileMenuDOM = document.createElement("div");
+   mobileMenuDOM.classList.add("mobile-menu");
+   mobileMenuDOM.addEventListener("click", event => {
+      event.stopPropagation();
+   });
+   mobileMenuDOM.append(headerMenu.querySelector("ul").cloneNode(true));
+   headerMenu.append(mobileMenuDOM);
+};
+
+const openMenu = () => {
+   if (mobileMenuDOM) {
+   } else {
+      createMobileMenu();
+   }
+   mobileMenuDOM.classList.add("open");
+};
+
+const toggleMobileMenu = event => {
+   if (isMenuOpen) {
+      closeMenu();
+   } else {
+      openMenu();
+   }
+   isMenuOpen = !isMenuOpen;
+};
+
+iconMobile.addEventListener("click", event => {
+     event.stopPropagation();
+   // console.log('iconmobile');
+   toggleMobileMenu();
+});
+
+window.addEventListener("click", () => {
+   if (isMenuOpen) {
+      toggleMobileMenu();
+   }
+});
+
+window.addEventListener("resize", event => {
+   if (window.innerWidth > 480 && isMenuOpen) {
+      toggleMobileMenu();
+   }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -4,60 +4,68 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: {
-    mainEntry : path.resolve(__dirname, "src/index.js"), // Important
-    form : path.resolve(__dirname, "src/form/form.js"),
-    topbar : path.resolve(__dirname, "src/assets/javascripts/topbar.js"),
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.scss$/i,
-        use: ["style-loader", "css-loader", 'sass-loader'] /* Les plugins positionnés dans "use" sont exécutés de droite à gauche */
+   entry: {
+      mainEntry: path.resolve(__dirname, "src/index.js"), //! Important
+      form: path.resolve(__dirname, "src/form/form.js"),
+      topbar: path.resolve(__dirname, "src/assets/javascripts/topbar.js"),
+   },
+   output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name].bundle.js"
+   },
 
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: "./src/assets/images",
-          to: "assets/images",
-        }
+   //   resolve: {
+   //    extensions: ['.js'],
+   //    modules: [path.resolve(__dirname, '.'), 'node_modules']
+   //   },
+
+   module: {
+      rules: [
+         {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+               loader: "babel-loader"
+            }
+         },
+         {
+            test: /\.scss$/i,
+            use: ["style-loader", "css-loader", 'sass-loader'] /* Les plugins positionnés dans "use" sont exécutés de droite à gauche */
+
+         }
       ]
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'index.html', // non obligatoire
-      template: path.resolve(__dirname, "src/index.html"), // Important
-      chunks: ["mainEntry", "topbar"]
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'form.html', // non obligatoire
-      template: path.resolve(__dirname, "src/form/form.html"),
-      chunks: ["form", 'topbar']
-    })
-  ],
-  // stats: "minimal",
-  devtool: "source-map",
-  mode: "development",
-  devServer: {
-    static: path.resolve(__dirname, './dist'),
-    open: true,
-    watchFiles: ['./src/**'], // Important
-    port: 4000,
-    hot: true,
-  }
+   },
+
+   plugins: [
+      new CleanWebpackPlugin(),
+      new CopyWebpackPlugin({
+         patterns: [
+            {
+               from: "./src/assets/images",
+               to: "assets/images",
+            }
+         ]
+      }),
+      new HtmlWebpackPlugin({
+         filename: 'index.html', //? non obligatoire
+         template: path.resolve(__dirname, "src/index.html"), //! Important
+         chunks: ["mainEntry", "topbar"]
+      }),
+      new HtmlWebpackPlugin({
+         filename: 'form.html', //? non obligatoire
+         template: path.resolve(__dirname, "src/form/form.html"),
+         chunks: ["form", 'topbar']
+      })
+   ],
+
+   //? stats: "minimal",
+   devtool: "source-map",
+   mode: "development",
+   devServer: {
+      static: path.resolve(__dirname, './dist'),
+      open: true,
+      watchFiles: ['./src/**'], //! Important
+      port: 4000,
+      hot: true,
+   }
 };
